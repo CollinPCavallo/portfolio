b8269e6d6d11e04b80144dd50b1fc0dc91e65778
Collin Cavallo <collincavallo@gmail.com>
2018-05-24 09:10:56 -0500

    Added better styling more contrast and new font

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-$('body').scrollspy({target: '#navbar-example2'})
+// emailjs.init("user_XH03tBioDXDRugxu9eBz6");
+// var service_id = 'gmail';
+// var template_id = 'contact_form';
+// var template_params = {
+//     name: $('#user_name').val,
+//     reply_email: $('#user_email').val,
+//     message: $('#text').val,
+//     text: $('#text').val
+// };
+// emailjs.send(service_id, template_id, template_params);
